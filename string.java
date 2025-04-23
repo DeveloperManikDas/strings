12347e5a0453d1bb9b7fc9f4ca59e243712e806d
DeveloperManikDas <developermanikdas@gmail.com>
2025-04-23 23:04:31 +0530

    add palindrome checker

---
+++ string.java
@@ -0,0 +1,20 @@
+public class string {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false; // Not a palindrome
+            }
+            left++;
+            right--;
+        }
+
+        return true; // It's a palindrome
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("mom"));
+    }
+}
