68e390ce4ad43fcffecf3433db498e51c41982e2
DeveloperManikDas <developermanikdas@gmail.com>
2025-05-10 23:58:49 +0530

    reverse vowel

--- string.java
+++ string.java
@@ -238,6 +238,30 @@ public class string {
         return new String(chars);
     }
 
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !isVowel(chars[left])) left++;
+            while (left < right && !isVowel(chars[right])) right--;
+
+            // Swap vowels
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(chars);
+    }
+
+    private boolean isVowel(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
     public static void main(String[] args) {
         // System.out.println(isPalindrome("mom"));
         // System.out.println(capitalizeEachWord("mom is good"));
